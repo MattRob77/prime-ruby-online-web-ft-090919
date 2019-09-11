4fff67273f1b1ba3f3d3534dd0fa9a723f8987a9
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-11 19:10:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,9 @@
-def prime?(numbers)
-  return false if numbers < 2
-end 
+def prime?(num)
+  if num <= 1
+    false
+  elsif num == 2
+    true
+  else
+    (2..num/2).none?{ |i| num % i == 0 }
+  end
+end
